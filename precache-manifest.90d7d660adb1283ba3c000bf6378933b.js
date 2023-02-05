30aa6f32a9e2d0fa8ad75524e64380c02f4e2e30
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-05 20:20:22 -0300

    Updates

---
+++ precache-manifest.90d7d660adb1283ba3c000bf6378933b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf312ca1380c4860135ae54b3e0dd14",
+    "url": "/planets-project/index.html"
+  },
+  {
+    "revision": "99b6b7cddec27c2b9805",
+    "url": "/planets-project/static/css/main.0d1215b3.chunk.css"
+  },
+  {
+    "revision": "d45ef77ebc8c1e25a765",
+    "url": "/planets-project/static/js/2.31506b98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/planets-project/static/js/2.31506b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b6b7cddec27c2b9805",
+    "url": "/planets-project/static/js/main.9cf9cf8b.chunk.js"
+  },
+  {
+    "revision": "ade291d366d2a4965813",
+    "url": "/planets-project/static/js/runtime-main.e5c5454c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/guide-react-gh-pages/planets-project/precache-manifest.84698f9caafc1b19e565698a4fdefbeb.js"
+  "/planets-project/precache-manifest.90d7d660adb1283ba3c000bf6378933b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guide-react-gh-pages/planets-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/planets-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
